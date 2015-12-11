cc990ff3fc98ad70c2b9976e33b98eac8e40f243
CJ DiMaggio <charlesdimaggio@localprojects.net>
2015-12-11 11:31:18 -0500

    First pass at getting twilio REST client setup

--- lib/logstash/outputs/twilio.rb
+++ lib/logstash/outputs/twilio.rb
@@ -3,14 +3,57 @@ require "logstash/outputs/base"
 require "logstash/namespace"
 
 class LogStash::Outputs::Twilio < LogStash::Outputs::Base
-  config_name "example"
+  config_name "twilio"
+
+  config :sid, :validate => :string, :required => true
+  config :secret, :validate => :string, :required => true
+  config :accountId, :validate => :string, :required => true
+  
+  config :from, :validate => :string, :required => true
+  config :to, :validate => :string, :required => true
+
+  config :format, :validate => :string, :default => "${message}"
 
   public
   def register
+  	require 'rest-client'
   end # def register
 
   public
   def receive(event)
-    return "Event received"
+    return unless output?(event)
+
+    text = event.sprintf(@format)
+
+    recipients = event.sprintf(@to).split(',')
+
+    url = "https://https://#{@sid}:#{@secret}@api.twilio.com/2010-04-01/Accounts/#{@accountId}/Messages"
+
+   	recipients.each do |recipient|
+
+	    payload = {
+	    	"From" => @from,
+	    	"To" => recipient,
+	    	"Body" => text
+	    }
+
+	    begin
+	      RestClient.post(
+	        url,
+	        payload,
+	        :accept => "application/xml",
+	        :'User-Agent' => "logstash-output-twilio",
+	        :content_type => "application/x-www-form-urlencoded") { |response, request, result, &block|
+	          if response.code != 200
+	            @logger.warn("Got a #{response.code} response: #{response}")
+	          end
+	        }
+	    rescue Exception => e
+	      @logger.warn("Unhandled exception", :exception => e,
+	                   :stacktrace => e.backtrace)
+	    end
+
+	end
+
   end # def event
 end # class LogStash::Outputs::Twilio
